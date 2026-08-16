9f13786c59951b3930a6b45e736d8795227b2cf8
Dominique Normand <dominique@normand.us>
2026-08-16 17:48:16 -0400

    Move stories to their own repo

--- main.ts
+++ main.ts
@@ -20,62 +20,38 @@ import { Quest18 } from './Quests/quest18.ts'
 import { Quest19 } from './Quests/quest19.ts'
 import { Quest20 } from './Quests/quest20.ts'
 
-import { Quest01 as Story1Quest01 } from './Story1/quest01.ts'
-import { Quest02 as Story1Quest02 } from './Story1/quest02.ts'
-import { Quest03 as Story1Quest03 } from './Story1/quest03.ts'
-
-import { Quest01 as Story2Quest01 } from './Story2/quest01.ts'
-import { Quest02 as Story2Quest02 } from './Story2/quest02.ts'
-import { Quest03 as Story2Quest03 } from './Story2/quest03.ts'
-
-import { Quest01 as Story3Quest01 } from './Story3/quest01.ts'
-import { Quest02 as Story3Quest02 } from './Story3/quest02.ts'
-import { Quest03 as Story3Quest03 } from './Story3/quest03.ts'
-
 const quests: IQuest[] = [
-  new Quest01(),
-  new Quest02(),
-  new Quest03(),
-  new Quest04(),
-  new Quest05(),
-  new Quest06(),
-  new Quest07(),
-  new Quest08(),
-  new Quest09(),
-  new Quest10(),
-  new Quest11(),
-  new Quest12(),
-  new Quest13(),
-  new Quest14(),
-  new Quest15(),
-  new Quest16(),
-  new Quest17(),
-  new Quest18(),
-  new Quest19(),
-  new Quest20(),
-
-  new Story1Quest01(),
-  new Story1Quest02(),
-  new Story1Quest03(),
-
-  new Story2Quest01(),
-  new Story2Quest02(),
-  new Story2Quest03(),
-
-  new Story3Quest01(),
-  new Story3Quest02(),
-  new Story3Quest03(),
+    new Quest01(),
+    new Quest02(),
+    new Quest03(),
+    new Quest04(),
+    new Quest05(),
+    new Quest06(),
+    new Quest07(),
+    new Quest08(),
+    new Quest09(),
+    new Quest10(),
+    new Quest11(),
+    new Quest12(),
+    new Quest13(),
+    new Quest14(),
+    new Quest15(),
+    new Quest16(),
+    new Quest17(),
+    new Quest18(),
+    new Quest19(),
+    new Quest20(),
 ]
 
 type TimeEntry = {
-  duration: number
-  message: string
+    duration: number
+    message: string
 }
 
 const times: { [id: string]: TimeEntry } = {}
 
 function compare(a: TimeEntry, b: TimeEntry): number {
-  return a.duration - b.duration
+    return a.duration - b.duration
 }
 
 const output = console.log
@@ -83,81 +59,83 @@ const output = console.log
 console.debug = () => {}
 
 const questLog = (buffer: string[]) => (msg: string) => {
-  buffer.push(msg)
+    buffer.push(msg)
 }
 
 console.time = (key: string) => {
-  if (key[0] == '@') {
-    performance.mark(key + '$start')
-  }
+    if (key[0] == '@') {
+        performance.mark(key + '$start')
+    }
 }
 
 console.timeLog = (key: string, msg: string) => {
-  if (key[0] == '@') {
-    performance.mark(key + '$end')
-    const t = performance.measure(key, key + '$start', key + '$end')
-    times[key] = {
-      duration: t.duration,
-      message: `${t.duration.toFixed(5)}ms ${msg}`,
+    if (key[0] == '@') {
+        performance.mark(key + '$end')
+        const t = performance.measure(key, key + '$start', key + '$end')
+        times[key] = {
+            duration: t.duration,
+            message: `${t.duration.toFixed(5)}ms ${msg}`,
+        }
     }
-  }
 }
 
 function format(value: string | number, length: number, direction: 'L' | 'R' = 'R') {
-  const s = String(value)
-  const sLen = s.length
-  if (sLen < length) {
-    const padding = ' '.repeat(length - sLen)
-    return direction === 'L' ? padding + s : s + padding
-  }
-  return s.substring(0, length)
+    const s = String(value)
+    const sLen = s.length
+    if (sLen < length) {
+        const padding = ' '.repeat(length - sLen)
+        return direction === 'L' ? padding + s : s + padding
+    }
+    return s.substring(0, length)
 }
 
 function executeAll() {
-  console.time('@advent-2025')
+    console.time('@advent-2025')
 
-  output('┌────────────────────────────────────────────────────────────────────────────────────────────────────────────────────┐')
-  output('│ 🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄 The Song of Ducks and Dragons - 2025 🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄 │')
-  output('├───────┬─────────────────────────┬──────────────────────┬─────────────────────────────────────────────┬─────────────┤')
-  output('│ Quest │ Title                   │ Part 1               │ Part 2               │ Part 3               │ Time in ms  │')
+    output('┌────────────────────────────────────────────────────────────────────────────────────────────────────────────────────┐')
+    output('│ 🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄 The Song of Ducks and Dragons - 2025 🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄 │')
+    output('├───────┬─────────────────────────┬──────────────────────┬─────────────────────────────────────────────┬─────────────┤')
+    output('│ Quest │ Title                   │ Part 1               │ Part 2               │ Part 3               │ Time in ms  │')
 
-  for (const quest of quests) {
-    const lines: string[] = []
-    console.log = questLog(lines)
-    const key = `@quest${quest.quest}`
-    const msg = `to execute all 3 parts of quest ${quest.quest}`
-    console.time(key)
-    quest.execute()
-    console.timeLog(key, msg)
+    for (const quest of quests) {
+        const lines: string[] = []
+        console.log = questLog(lines)
+        const key = `@quest${quest.quest}`
+        const msg = `to execute all 3 parts of quest ${quest.quest}`
+        console.time(key)
+        quest.execute()
+        console.timeLog(key, msg)
 
-    const p1 = lines[1].split('Part 1: ')[1]
-    const p2 = lines[2].split('Part 2: ')[1]
-    const p3 = lines[3].split('Part 3: ')[1]
-    const duration = times[`@quest${quest.quest}`].duration
-    const ms = `${duration.toFixed(4)} ms`
+        const p1 = lines[1].split('Part 1: ')[1]
+        const p2 = lines[2].split('Part 2: ')[1]
+        const p3 = lines[3].split('Part 3: ')[1]
+        const duration = times[`@quest${quest.quest}`].duration
+        const ms = `${duration.toFixed(4)} ms`
 
-    output('├───────┼─────────────────────────┼──────────────────────┼──────────────────────┼──────────────────────┼─────────────┤')
-    output(`│ ${format(quest.quest, 5, 'L')} │ ${format(quest.title, 23)} │ ${format(p1, 20)} │ ${format(p2, 20)} │ ${format(p3, 20)} │ ${format(ms, 11, 'L')} │`)
-  }
-  console.timeLog('@advent-2025', 'to execute them all')
+        output('├───────┼─────────────────────────┼──────────────────────┼──────────────────────┼──────────────────────┼─────────────┤')
+        output(
+            `│ ${format(quest.quest, 5, 'L')} │ ${format(quest.title, 23)} │ ${format(p1, 20)} │ ${format(p2, 20)} │ ${format(p3, 20)} │ ${format(ms, 11, 'L')} │`,
+        )
+    }
+    console.timeLog('@advent-2025', 'to execute them all')
 
-  const total = `${times['@advent-2025'].duration.toFixed(4)} ms`
+    const total = `${times['@advent-2025'].duration.toFixed(4)} ms`
 
-  output('└───────┴─────────────────────────┴──────────────────────┴──────────────────────┴──────────────────────┼─────────────┤')
-  output(`                                                                                                       │ ${format(total, 11, 'L')} │`)
-  output('                                                                                                       └─────────────┘')
+    output('└───────┴─────────────────────────┴──────────────────────┴──────────────────────┴──────────────────────┼─────────────┤')
+    output(`                                                                                                       │ ${format(total, 11, 'L')} │`)
+    output('                                                                                                       └─────────────┘')
 
-  times['@advent-2025'].duration = 0 // For the sorting
+    times['@advent-2025'].duration = 0 // For the sorting
 
-  const order = Object.keys(times)
-    .filter(k => k !== '@advent-2025')
-    .filter(k => times[k].duration > 10)
-    .sort((a: string, b: string) => compare(times[b], times[a]))
+    const order = Object.keys(times)
+        .filter(k => k !== '@advent-2025')
+        .filter(k => times[k].duration > 10)
+        .sort((a: string, b: string) => compare(times[b], times[a]))
 
-  for (const key of order) {
-    output(times[key].message)
-  }
-  output('\r')
+    for (const key of order) {
+        output(times[key].message)
+    }
+    output('\r')
 }
 
 executeAll()
--- quest.ts
+++ quest.ts
@@ -1,76 +1,74 @@
 export interface IQuest {
-  quest: number
-  title: string
-  execute(): void
+    quest: number
+    title: string
+    execute(): void
 }
 
 console.log(Deno.cwd())
 const dataPath = Deno.cwd().endsWith('/2025') ? '.' : '..'
 
 export abstract class Quest<T> implements IQuest {
-  public quest: number
-  public title: string
-  private folder: string
-  private encoder: TextEncoder | undefined = undefined
+    public quest: number
+    public title: string
+    private folder: string
+    private encoder: TextEncoder | undefined = undefined
 
-  writeSync(data: string): void {
-    if (this.encoder === undefined) {
-      this.encoder = new TextEncoder()
+    writeSync(data: string): void {
+        if (this.encoder === undefined) {
+            this.encoder = new TextEncoder()
+        }
+        const buffer = this.encoder.encode(data)
+
+        Deno.stdout.writeSync(buffer)
     }
-    const buffer = this.encoder.encode(data)
 
-    Deno.stdout.writeSync(buffer)
-  }
-
-  constructor(quest: number, story: number = 0, title: string | undefined = undefined) {
-    this.quest = quest
-    this.title = title ?? `${story ? `Story ${story}, Quest ` : 'Quest '} ${quest}`
-    this.folder = story ? `Story${story}/data/quest${this.quest}` : `Quests/data/quest${this.quest}`
-  }
-
-  abstract part1(input: T): number | string
-  abstract part2(input: T): number | string
-  abstract part3(input: T): number | string
-  abstract loadInput(part: number): T
-
-  timeStart(name: string) {
-    const key = name //.toLowerCase().replace('  ', '-')
-    console.time(`quest${this.quest}:${key}`)
-  }
-
-  timeEnd(name: string) {
-    const key = name //.toLowerCase().replace('  ', '-')
-    console.timeLog(`quest${this.quest}:${key}`, `to ${name === 'input' ? 'parse' : 'execute'} ${name} of quest ${this.quest}`)
-  }
-
-  readDataFile(part: number): string[] {
-    const data = Deno.readTextFileSync(`${dataPath}/${this.folder}/${part}.raw`)
-    return data.split('\n')
-  }
-
-  execute(): void {
-    try {
-      console.log(`--- quest ${this.quest}: ${this.title} ---`)
-
-      this.timeStart('total')
-
-      this.timeStart('part-1')
-      console.log(`Part 1: ${this.part1(this.loadInput(1))}`)
-      this.timeEnd('part-1')
-
-      this.timeStart('part-2')
-      console.log(`Part 2: ${this.part2(this.loadInput(2))}`)
-      this.timeEnd('part-2')
-
-      this.timeStart('part-3')
-      console.log(`Part 3: ${this.part3(this.loadInput(3))}`)
-      this.timeEnd('part-3')
-
-      this.timeEnd('total')
-    } catch (error) {
-      // deno-lint-ignore no-debugger
-      debugger
-      throw error
+    constructor(quest: number, title: string | undefined = undefined) {
+        this.quest = quest
+        this.title = title ?? `Quest ${quest}`
+        this.folder = `Quests/data/quest${this.quest}`
     }
-  }
+
+    abstract part1(input: T): number | string
+    abstract part2(input: T): number | string
+    abstract part3(input: T): number | string
+    abstract loadInput(part: number): T
+
+    timeStart(name: string) {
+        const key = name //.toLowerCase().replace('  ', '-')
+        console.time(`quest${this.quest}:${key}`)
+    }
+
+    timeEnd(name: string) {
+        const key = name //.toLowerCase().replace('  ', '-')
+        console.timeLog(`quest${this.quest}:${key}`, `to ${name === 'input' ? 'parse' : 'execute'} ${name} of quest ${this.quest}`)
+    }
+
+    readDataFile(part: number): string[] {
+        const data = Deno.readTextFileSync(`${dataPath}/${this.folder}/${part}.raw`)
+        return data.split('\n')
+    }
+
+    execute(): void {
+        try {
+            console.log(`--- quest ${this.quest}: ${this.title} ---`)
+
+            this.timeStart('total')
+
+            this.timeStart('part-1')
+            console.log(`Part 1: ${this.part1(this.loadInput(1))}`)
+            this.timeEnd('part-1')
+
+            this.timeStart('part-2')
+            console.log(`Part 2: ${this.part2(this.loadInput(2))}`)
+            this.timeEnd('part-2')
+
+            this.timeStart('part-3')
+            console.log(`Part 3: ${this.part3(this.loadInput(3))}`)
+            this.timeEnd('part-3')
+
+            this.timeEnd('total')
+        } catch (error) {
+            throw error
+        }
+    }
 }
